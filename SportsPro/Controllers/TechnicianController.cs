d9f0e04029506b96d298eb38f8ef996fc3b14300
mario.montes <mario.montes@PROGRAM21>
2025-02-01 23:16:01 -0600

    Added technician controller

---
+++ SportsPro/Controllers/TechnicianController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsPro.Controllers
+{
+    public class TechnicianController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
