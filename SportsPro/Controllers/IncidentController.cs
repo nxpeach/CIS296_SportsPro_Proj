c1fa8d233063c6d402e0643488a1e2425189469c
mario.montes <mario.montes@PROGRAM21>
2025-02-08 00:15:09 -0600

    Added code to the Incident controller and the corresponding view to have page work on the site.

--- SportsPro/Controllers/IncidentController.cs
+++ SportsPro/Controllers/IncidentController.cs
@@ -16,6 +16,56 @@ namespace SportsPro.Controllers
             context = ctx;
         }
 
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Customers = context.Customers.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ToList();
+            ViewBag.Products = context.Products.OrderBy(p => p.Name).ToList();
+            ViewBag.Technicians = context.Technicians.OrderBy(t => t.Name).ToList();
+            return View("Edit", new Incident());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Customers = context.Customers.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ToList();
+            ViewBag.Products = context.Products.OrderBy(p => p.Name).ToList();
+            ViewBag.Technicians = context.Technicians.OrderBy(t => t.Name).ToList();
+            var incident = context.Incidents.Find(id);
+            return View(incident);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                if (incident.IncidentID == 0)
+                {
+                    context.Incidents.Add(incident);
+                }
+                else
+                {
+                    context.Incidents.Update(incident);
+                }
+
+                context.SaveChanges();
+                return RedirectToAction("List");
+
+            }
+            else
+            {
+                ViewBag.Action = (incident.IncidentID == 0) ? "Add" : "Edit";
+                ViewBag.Customers = context.Customers.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ToList();
+                ViewBag.Products = context.Products.OrderBy(p => p.Name).ToList();
+                ViewBag.Technicians = context.Technicians.OrderBy(t => t.Name).ToList();
+                return View(incident);
+            }
+        }
+
         public IActionResult List()
         {
             var incidents = context.Incidents.Include(i => i.Customer).
