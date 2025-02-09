ec534b318066423ad30e5d5e01f6c9036f2efb02
Victoria <150954681+nxpeach@users.noreply.github.com>
2025-02-09 11:54:25 -0600

    Fixed title error on delete confirmation view, fixed country selection invalid

---
+++ SportsPro/Controllers/CustomerController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using System.Linq;
+
+namespace SportsPro.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly SportsProContext _context;
+
+        public CustomerController(SportsProContext context)
+        {
+            _context = context;
+        }
+
+        //GET Customer/List
+        public IActionResult List()
+        {
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
+
+        //GET Customer/Add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Countries = _context.Countries.ToList(); //Get countries for dropdown
+            return View("AddEdit", new Customer());
+        }
+
+        //POST Customer/Add
+        [HttpPost]
+        public IActionResult Add(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            ViewBag.Countries = _context.Countries.ToList(); //Get countries again if validation fails
+            return View(customer);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Countries = _context.Countries.ToList();
+            var customer = _context.Customers.Find(id);
+            if (customer == null)   //If there is no customer, return the not found response
+            {
+                return NotFound();
+            }
+            return View("AddEdit", customer);
+        }
+
+        //POST Customer/Edit/
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Update(customer);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            ViewBag.Countries = _context.Countries.ToList();
+            return View(customer);
+        }
+
+        //GET Customer/Delete/
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        //POST Customer/Delete/
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+
+        //SAVE the new customer and add
+        [HttpPost]
+        public IActionResult Save(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                if (customer.CustomerID == 0) //If this is a new customer
+                {
+                    _context.Customers.Add(customer);
+                }
+                else //Update existing customer
+                {
+                    _context.Customers.Update(customer);
+                }
+                _context.SaveChanges();
+                return RedirectToAction("List");    //Rerturn the list view
+            }
+            ViewBag.Action = customer.CustomerID == 0 ? "Add" : "Edit"; //Get the correct page name
+
+            ViewBag.Countries = _context.Countries.ToList(); //Make sure countries are available if validation fails
+            return View("AddEdit", customer);
+        }
+
+    }
+}
--- SportsPro/Models/Customer.cs
+++ SportsPro/Models/Customer.cs
@@ -1,20 +1,34 @@
-﻿namespace SportsPro.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsPro.Models
 {
     public class Customer
     {
 		public int CustomerID { get; set; }
 
+		[Required(ErrorMessage = "A first name is required.")]
 		public string FirstName { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "A last name is required.")]
 		public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "An address is required.")]
 		public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A city is required.")]
 		public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A state is required.")]
 		public string State { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A postal code is required.")]
 		public string PostalCode { get; set; } = string.Empty;
 		public string? Phone { get; set; } 
-        public string? Email { get; set; } 
+        public string? Email { get; set; }
 
-        public string CountryID { get; set; } = string.Empty;   // foreign key property
-        public Country Country { get; set; } = null!;           // navigation property
+        [Required(ErrorMessage = "A country is required.")]
+        public string CountryID { get; set; } = string.Empty;
+        public Country? Country { get; set; }
 
         public string FullName => FirstName + " " + LastName;   // read-only property
 	}
